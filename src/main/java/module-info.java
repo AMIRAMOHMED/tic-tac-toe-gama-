3004214ab77c4b8f486251370c14412000c03850
AMIRAMOHMED <amiramohmaed887@gmail.com>
2024-07-20 11:12:31 +0300

    first

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.tictactoegama {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.tictactoegama to javafx.fxml;
+    exports com.example.tictactoegama;
+}
