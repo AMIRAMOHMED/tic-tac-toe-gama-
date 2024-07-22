7552c714d2802b00fae28cdb360579a856157549
amira <amiramohmaed887@gmail.com>
2024-07-22 17:05:47 +0300

    edit gamaLogic

--- src/main/java/com/example/tictactoegama/logic/GameLogic.java
+++ src/main/java/com/example/tictactoegama/logic/GameLogic.java
@@ -14,33 +14,21 @@ public class GameLogic {
         this.aiMoodOption = aiMoodOption;
     }
 
-    public void processPlayerMove(Button clickedButton, int row, int col, String currentPlayer) {
-        clickedButton.setText(currentPlayer);
-        updateButtonStyle(clickedButton, currentPlayer);
-        playBoard.play(row, col, currentPlayer.charAt(0));
+    public void processPlayerMove(Button clickedButton, int row, int col, char currentPlayer) {
+        clickedButton.setText(String.valueOf(currentPlayer));
+        updateButtonStyle(clickedButton, String.valueOf(currentPlayer));
+        playBoard.play(row, col, currentPlayer);
     }
 
-    public void processComputerMove(String computerSymbol) {
-        aiMoodOption.makeMove(playBoard, computerSymbol.charAt(0));
+    public void processComputerMove(char computerSymbol) {
+        aiMoodOption.makeMove(playBoard, computerSymbol);
     }
 
     public boolean checkForWin(int row, int col, char symbol) {
         return playBoard.checkWin(row, col, symbol) != -1;
     }
 
-    public boolean checkForWinAfterComputerMove(String computerSymbol) {
-        char[][] board = playBoard.getBoard();
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == computerSymbol.charAt(0)) {
-                    if (checkForWin(i, j, computerSymbol.charAt(0))) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
+
 
     private void updateButtonStyle(Button button, String symbol) {
     }
